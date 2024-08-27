acebc877fc8b0974639d6745d8428eed15dd4ac5
Carlijavier5 <carlijavier5@gmail.com>
2024-08-27 10:31:59 -0400

    Organization Push

--- Assets/_Scripts/Settings/DefaultSummonProperties.cs
+++ Assets/_Scripts/Settings/DefaultSummonProperties.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 
 [CreateAssetMenu()]
 public class DefaultSummonProperties : ScriptableObject {
     public Material fadeMaterial;
     public AnimationCurve growthCurveXZ, growthCurveY;
 }
+#endif
