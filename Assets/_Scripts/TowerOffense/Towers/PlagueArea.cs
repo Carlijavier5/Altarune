b4b4333556a168d075d2614a08e4dc7c34d0ff8e
Merannix <75506999+Merannix@users.noreply.github.com>
2024-09-07 02:25:36 -0400

    Initial Setup

---
+++ Assets/_Scripts/TowerOffense/Towers/PlagueArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlagueArea : MonoBehaviour {
+
+    [SerializeField] private float growSpeed;
+    [SerializeField] private float timeScale;
+    private Vector3 targetScale;
+
+    void Awake() {
+        targetScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
+    void Update() {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, growSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent(out Entity entity)) {
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.TryGetComponent(out Entity entity)) {
+            
+        }
+    }
+}
