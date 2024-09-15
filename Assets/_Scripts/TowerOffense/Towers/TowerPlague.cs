dcaa19a4a886dc3fd3b1e0aa7618f66f123ae8a7
Merannix <75506999+Merannix@users.noreply.github.com>
2024-09-15 15:04:03 -0400

    Implemented Plague Tower

---
+++ Assets/_Scripts/TowerOffense/Towers/TowerPlague.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TowerPlague : Summon {
+
+    [SerializeField] private PlagueArea plagueArea;
+    [SerializeField] private Transform launchPoint;
+    private bool init;
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    public override void Init() {
+        init = true;
+        SpawnPlagueArea();
+    }
+
+    void Update() {
+        if (!init) return;
+    }
+
+    private void SpawnPlagueArea() {
+        Instantiate(plagueArea, launchPoint.transform.position, Quaternion.identity);
+    }
+}
