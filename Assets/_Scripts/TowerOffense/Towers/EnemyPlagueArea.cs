9afb2323de705c3932db595ff682de9f41462923
Merannix <75506999+Merannix@users.noreply.github.com>
2024-09-18 13:04:31 -0400

    Optimized Plague Tower

--- Assets/_Scripts/TowerOffense/Towers/EnemyPlagueArea.cs
+++ Assets/_Scripts/TowerOffense/Towers/EnemyPlagueArea.cs
@@ -7,61 +7,46 @@ public class EnemyPlagueArea : MonoBehaviour {
     [SerializeField] private PlagueStatusEffect plagueEffect;
     [SerializeField] private float growSpeed;
     [SerializeField] private float timeScale;
-    [SerializeField] private float plagueInterval;
     [SerializeField] private float fadeDuration;
     [SerializeField] private float range;
     private Vector3 targetScale;
     private Renderer rend;
     public int parentID;
-    private List<Collider> enemiesInArea = new List<Collider>();
 
     void Awake() {
         rend = GetComponent<Renderer>();
         targetScale = Vector3.Scale(transform.localScale, new Vector3(range, 1.0f, range));
         transform.localScale = Vector3.zero;
+        StartCoroutine(ExpandArea());
     }
 
     void OnEnable() {
-        rend.material.SetFloat("_Alpha", 0.7f);
-        transform.localScale = Vector3.zero;
+        StartCoroutine(ExpandArea());
     }
-
-    void Update() {
-        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, growSpeed * Time.deltaTime);
-        if(transform.localScale == targetScale) {
-            StartCoroutine(InfectEnemies());
-            StartCoroutine(FadeOut(0f, fadeDuration));
-        }
-    }
-
+ 
     void OnTriggerEnter(Collider other) {
         if (other.TryGetComponent(out Entity entity) && entity.Faction == EntityFaction.Hostile && entity.GetInstanceID() != parentID) {
-            if(!enemiesInArea.Contains(other)) {
-                enemiesInArea.Add(other);
+                InfectEnemy(other);
+        }
+    }
+
+    private void InfectEnemy(Collider enemy) {
+        Entity entity = enemy.GetComponent<Entity>();
+        entity.ApplyEffects(new[] {plagueEffect.Clone() });
+    }
+
+    private IEnumerator ExpandArea() {
+        while (transform.localScale != targetScale) {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, growSpeed * Time.deltaTime);
+            if (transform.localScale == targetScale) {
+                StartCoroutine(FadeOut(0f, fadeDuration));
             }
-        }
-    }
-
-    void OnTriggerExit(Collider other) {
-        if (enemiesInArea.Contains(other)) {
-            enemiesInArea.Remove(other);
-        }
-    }
-
-    private IEnumerator InfectEnemies() {
-            foreach (Collider enemy in enemiesInArea) {
-                if (enemy != null) {
-                    Entity entity = enemy.GetComponent<Entity>();
-                    entity.ApplyEffects(new[] {plagueEffect.Clone() });
-                }
-            yield return new WaitForSeconds(plagueInterval);
+            yield return null;
         }
     }
 
     private IEnumerator FadeOut(float targetAlpha, float duration) {
-        
         float initialAlpha = rend.material.GetFloat("_Alpha");
-
         float timeElapsed = 0f;
 
         while (timeElapsed < duration) {
@@ -69,8 +54,8 @@ public class EnemyPlagueArea : MonoBehaviour {
             rend.material.SetFloat("_Alpha", Mathf.Lerp(initialAlpha, targetAlpha, timeElapsed / duration));
             yield return null;
         }
-        rend.material.SetFloat("_Alpha", targetAlpha);
-        enemiesInArea.Clear();
+        transform.localScale = Vector3.zero;
+        rend.material.SetFloat("_Alpha", 0.7f);
         this.gameObject.SetActive(false);
     }
 }
