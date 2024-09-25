9099c238b64f7214c486defd2ab179f5d266eac4
yilohdj <yilohdj@gmail.com>
2024-09-24 20:36:30 -0400

    Updated framework of FireTrap
    Changed behavior to damage all entities within collider after specific amount of time

--- Assets/_Scripts/ObjectBehaviors/FireTrap.cs
+++ Assets/_Scripts/ObjectBehaviors/FireTrap.cs
@@ -5,31 +5,28 @@ using UnityEngine;
 public class FireTrap : MonoBehaviour
 {
     private float damageTimer = 0.0f;
-    public float waitTime = 1.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField]
+    private float waitTime = 1.0f;
+    private HashSet<GameObject> entitiesInTrap = new HashSet<GameObject>();
     // Update is called once per frame
     void Update()
     {
-        if(damageTimer>waitTime){
-            Debug.Log("Damage Yan");
-            damageTimer = 0.0f;
+        if (entitesInTrap.Any()) {
+            damageTimer += Time.deltaTime; // Increment damageTimer if any entity is within FireTrap collider
         }
-    }
-    private void OnTriggerStay(Collider collider){
-        if (collider.gameObject.layer == 3){
-            damageTimer += Time.deltaTime;
-            
+        else {
+            damageTimer = 0.0f; // Reset damageTimer if there are no entitites
         }
+        if (damageTimer >= waitTime){
+            foreach (GameObject entity in entitiesInTrap) {
+                Debug.Log("Do Damage"); // Damage all entitites within collider when damageTimer reaches threshold
+            }
+        } 
     }
-    private void OnTriggerExit(Collider collider){
-        if(collider.gameObject.layer == 3){
-            damageTimer = 0.0f;
-        }
+    private void OnTriggerEnter(Collider other) {
+        entitiesInTrap.Add(other.gameObject); // Add entity to list that enters collision box
     }
-
+    private void OnTriggerExit(Collider other) {
+        entitiesInTrap.Remove(other.gameObject); // Remove entity from list that leaves collision box
+    }
 }
