d1a3718ef70686cf5f5ef42d7a2d2f9e0b1df743
Nathan Xie <nathankeystone@hotmail.com>
2024-09-26 16:04:31 -0400

    Added aggro range to FriendlyFireBigProjectile
    Added a aggro range to the friendly fire tower in which it targets the closest target an fires and does not fire if there is no target.

--- Assets/Scenes/Nathan Xie/FriendlyFireTower.cs
+++ Assets/Scenes/Nathan Xie/FriendlyFireTower.cs
@@ -2,39 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
-public class FriendlyFireTower : Summon
-{
+public class FriendlyFireTower : Summon{
     [SerializeField] private FriendlyFireBigProjectile projectilePrefab;
     [SerializeField] private float attackInterval;
     [SerializeField] private int minRandAngle;
     [SerializeField] private int maxRandAngle;
+    [SerializeField] private AggroRange aggroRange;
 
     private float attackTick = -0.2f;
-    private GameObject player = null;
     private bool init;
 
     protected override void Awake(){
         base.Awake();
-        if(player == null){
-            player = FindObjectOfType<PlayerController>().gameObject;
-        }
     }
 
     public override void Init() => init = true;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         Target();
     }
+    /// <summary>
+    /// Calculates the closest entity within it's aggro range and launches a FriendlyFireBigProjectile toward the entity with a random spread
+    /// </summary>
     private void Target(){
+        Vector3 closestVector = new Vector3(float.MaxValue, float.MaxValue, Int32.MaxValue);
         if(!init) return;
         attackTick += Time.deltaTime;
-        if (attackTick >= attackInterval) {
+        if (attackTick >= attackInterval && aggroRange.AggroTargets.Count != 0) {
+            foreach(Entity i in aggroRange.AggroTargets){
+                Vector3 currVector = i.transform.position - this.transform.position;
+                if(currVector.magnitude <= closestVector.magnitude) {
+                    closestVector = currVector;
+                }
+            }
             System.Random rand = new System.Random();
-            Vector3 playerVector = player.transform.position - this.transform.position;
-            float angle = Vector3.SignedAngle(playerVector, transform.right, Vector3.up);
+            float angle = Vector3.SignedAngle(closestVector, transform.right, Vector3.up);
             float randomAngle = rand.Next(minRandAngle,maxRandAngle);
             FriendlyFireBigProjectile projectile = Instantiate(projectilePrefab, transform.position + Vector3.up, Quaternion.identity);
             Quaternion myRotation = Quaternion.AngleAxis(-angle + randomAngle, Vector3.up);
