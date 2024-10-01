004bd2e888ba62ddd16f890fcb7c180fc53f3393
Ashwagandhae <julianlianbauer@gmail.com>
2024-10-01 18:55:49 -0400

    add armadillo

---
+++ Assets/Scenes/Julian/Scripts/RollStatusEffect.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class RollStatusEffect : StatusEffect
+{
+  [SerializeField]
+  private float defenseAddMod;
+
+
+  private bool stop = false;
+
+  [SerializeField] private Material material;
+  public override void Apply(Entity entity, bool isNew)
+  {
+    AttributeMods = new() { defense = { addMod = defenseAddMod } };
+  }
+
+  public override void Terminate(Entity entity)
+  {
+  }
+
+  public void Stop()
+  {
+    stop = true;
+  }
+
+  public override bool Update(Entity entity)
+  {
+    return stop;
+  }
+}
