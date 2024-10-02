4d89e47463f620fff2b25dde825b493112b9a56f
yilohdj <yilohdj@gmail.com>
2024-10-01 20:02:55 -0400

    added damage to firetrap

--- Assets/_Scripts/ObjectBehaviors/FireTrap.cs
+++ Assets/_Scripts/ObjectBehaviors/FireTrap.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,26 +6,34 @@ public class FireTrap : MonoBehaviour
     private float damageTimer = 0.0f;
     [SerializeField]
     private float waitTime = 1.0f;
-    private HashSet<GameObject> entitiesInTrap = new HashSet<GameObject>();
+    private HashSet<Entity> entitiesInTrap = new HashSet<Entity>();
     // Update is called once per frame
-    void Update()
-    {
+    [SerializeField]
+    private EntityFaction faction = EntityFaction.Friendly;
+    void Update() {
         if (entitiesInTrap.Count == 0) {
             damageTimer += Time.deltaTime; // Increment damageTimer if any entity is within FireTrap collider
-        }
-        else {
+        } else {
             damageTimer = 0.0f; // Reset damageTimer if there are no entitites
         }
-        if (damageTimer >= waitTime){
-            foreach (GameObject entity in entitiesInTrap) {
-                Debug.Log("Do Damage"); // Damage all entitites within collider when damageTimer reaches threshold
+        if (damageTimer >= waitTime) {
+            foreach (Entity entity in entitiesInTrap) {
+                if (entity.Faction == faction) {
+                    entity.TryDamage(1);
+                }
             }
-        } 
+        }
     }
+
     private void OnTriggerEnter(Collider other) {
-        entitiesInTrap.Add(other.gameObject); // Add entity to list that enters collision box
+        if (other.TryGetComponent(out Entity entity)) {
+            entitiesInTrap.Add(entity); // Add entity to list that enters collision box
+        }
     }
+
     private void OnTriggerExit(Collider other) {
-        entitiesInTrap.Remove(other.gameObject); // Remove entity from list that leaves collision box
+        if (other.TryGetComponent(out Entity entity)) {
+        entitiesInTrap.Remove(entity); // Remove entity from list that leaves collision box
+        }
     }
 }
