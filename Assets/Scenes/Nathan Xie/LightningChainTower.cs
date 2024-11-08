f2598a52c3bcccd125255ff5427ac331e4a621ba
Nathan Xie <nathankeystone@hotmail.com>
2024-11-08 15:24:54 -0500

    Added range to LightningChainTower
    added range to LightningChainTower.

--- Assets/Scenes/Nathan Xie/LightningChainTower.cs
+++ Assets/Scenes/Nathan Xie/LightningChainTower.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class LightningChainTower : Summon
 {
     [SerializeField] private LightningChainBall ball;
     [SerializeField] private float attackInterval;
+    [SerializeField] private AggroRange aggro;
     private bool init;
     private float attackTick = -0.2f;
     private Player target;
@@ -27,7 +29,14 @@ public class LightningChainTower : Summon
     private void Fire(){
         if (!init) return;
         attackTick += Time.deltaTime;
-        if (attackTick >= attackInterval && balls.Count < 2) {
+        if (attackTick >= attackInterval && balls.Count < 2 && aggro.AggroTargets.Count != 0) {
+            Vector3 closestVector = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            foreach(Entity i in aggro.AggroTargets) {
+                Vector3 currVector = i.transform.position - this.transform.position;
+                if(currVector.magnitude <= closestVector.magnitude) {
+                    closestVector = currVector;
+                }
+            }
             attackTick = 0;
             LightningChainBall projectile = Instantiate(ball, transform.position + Vector3.up, Quaternion.identity);
             projectile.OnTimeOut += RemoveBall;
