1e0b7aecbcec90d87e57c5f8dabfb6f7159bc877
Carlijavier5 <carlijavier5@gmail.com>
2024-11-17 16:36:36 -0500

    Scaramite Controller Bias

--- Assets/_Scripts/Utils/PathfindingUtils.cs
+++ Assets/_Scripts/Utils/PathfindingUtils.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public static class PathfindingUtils {
 
-    public static bool FindRandomRoamingPoint(Vector3 source, float distance, int attempts, out Vector3 clearPoint) {
-        int i = 0;
-        while (i < attempts) {
-            if (FindRandomRoamingPoint(source, distance, out clearPoint)) {
-                return true;
-            } i++;
-        }
-        clearPoint = Vector3.zero;
-        return false;
-    }
-
-    public static bool FindRandomRoamingPoint(Vector3 source, float distance, out Vector3 clearPoint) {
-        float dirAngle = Random.Range(0, 360);
+    /// <summary>
+    /// Find a clear roaming point around a source;
+    /// </summary>
+    /// <param name="source"> Origin of the search cast; </param>
+    /// <param name="distance"> Distance of the cast; </param>
+    /// <param name="angleRange"> Range of direction angles to pick; </param>
+    /// <param name="clearPoint"> Output clear point, if one is found; </param>
+    /// <returns> Whether a clear roaming point was found; </returns>
+    public static bool FindBiasedRoamingPoint(Vector3 source, float distance, Vector2 angleRange, out Vector3 clearPoint) {
+        float dirAngle = Random.Range(angleRange.x, angleRange.y);
+        dirAngle = SpatialUtils.WrapAngle360(dirAngle);
         Vector3 roamDir = AngleCast(source, distance, dirAngle) ? AngleToXZDirection(dirAngle)              /// Front
                         : AngleCast(source, distance, dirAngle + 180) ? AngleToXZDirection(dirAngle + 180)  /// Back
                         : AngleCast(source, distance, dirAngle + 90) ? AngleToXZDirection(dirAngle + 90)    /// Right
@@ -31,12 +29,35 @@ public static class PathfindingUtils {
         return roamDir.magnitude > 0;
     }
 
+    public static bool FindBiasedRoamingPoint(Vector3 source, float distance, Vector2 angleRange,
+                                              int attempts, out Vector3 clearPoint) {
+        int i = 0;
+        while (i < attempts) {
+            if (FindBiasedRoamingPoint(source, distance, angleRange, out clearPoint)) {
+                return true;
+            }
+            i++;
+        }
+        clearPoint = Vector3.zero;
+        return false;
+    }
+
+    public static bool FindRandomRoamingPoint(Vector3 source, float distance, int attempts, out Vector3 clearPoint) {
+        return FindBiasedRoamingPoint(source, distance, new Vector2(0, 360), attempts, out clearPoint);
+    }
+
+    public static bool FindRandomRoamingPoint(Vector3 source, float distance, out Vector3 clearPoint) {
+        return FindBiasedRoamingPoint(source, distance, new Vector2(0, 360), out clearPoint);
+    }
+
     private static bool AngleCast(Vector3 source, float distance, float angle) {
         Vector3 dir = AngleToXZDirection(angle);
         return !Physics.Raycast(source, dir, distance, LayerUtils.EnvironmentLayerMask);
     }
 
     private static Vector3 AngleToXZDirection(float angle) {
-        return new(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+        angle = SpatialUtils.WrapAngle360(angle);
+        return new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), 0,
+                           Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
     }
 }
