defccb87858fc07a89ba434a59e6750e849f02f8
Carlijavier5 <carlijavier5@gmail.com>
2024-11-29 15:06:55 -0500

    Tower Wheel Integration

--- Assets/_Scripts/UI/TowerWheel.cs
+++ Assets/_Scripts/UI/TowerWheel.cs
@@ -6,30 +6,24 @@ using UnityEngine.UI;
 public class TowerWheel : MonoBehaviour
 {
     [SerializeField] private SummonController summonController;
+    [SerializeField] private TowerWheelSlot[] towerSlots;
     [SerializeField] private Animator animator;
-    [SerializeField] private Transform bigWheel;
-    [SerializeField] private Transform smallWheel;
-    [SerializeField] private Sprite defaultBg;
-    [SerializeField] private Sprite selectBg;
+    [SerializeField] private Transform bigWheel, smallWheel;
+    [SerializeField] private float rotateDuration;
 
     private int currentSelectionIndex = 0;
 
     private readonly Dictionary<SummonData, int> summonAngleDict = new();
     private readonly Dictionary<SummonData, int> summonIndexDict = new();
 
-    private void Start() {
-        summonController.OnSummonSelected += HandleSummonSelected;
-        summonController.OnRosterSetup += HandleRosterSetup;
-    }
-
-    private void OnEnable() {
+    void OnEnable() {
         if (summonController != null) {
             summonController.OnSummonSelected += HandleSummonSelected;
             summonController.OnRosterSetup += HandleRosterSetup;
         }
     }
 
-    private void OnDisable() {
+    void OnDisable() {
         if (summonController != null) {
             summonController.OnSummonSelected -= HandleSummonSelected;
             summonController.OnRosterSetup -= HandleRosterSetup;
@@ -46,7 +40,8 @@ public class TowerWheel : MonoBehaviour
         int i = 0;
         foreach (SummonData d in data) {
             if (i < 8) {
-                bigWheel.GetChild(i).gameObject.SetActive(true);
+                towerSlots[i].Init(d);
+                towerSlots[i].Toggle(true);
             }
 
             if (summonAngleDict.ContainsKey(d)) {
@@ -60,7 +55,7 @@ public class TowerWheel : MonoBehaviour
         }
 
         for (int j = i; j < 8; j++) {
-            bigWheel.GetChild(j).gameObject.SetActive(false);
+            towerSlots[i].Toggle(false);
         }
 
         summonIndexDict.Clear();
@@ -76,44 +71,44 @@ public class TowerWheel : MonoBehaviour
             summonIndexDict.Add(d, i);
             i++;
         }
-
-        i = 0;
-        foreach (Transform t in bigWheel.transform) {
-            if (i >= data.Length) {
-                break;
-            }
-            t.GetChild(0).GetComponent<Image>().sprite = data[i].icon;
-            i++;
-        }
     }
 
     private Coroutine rotateWheelCoroutine;
 
     private void HandleSummonSelected(SummonType type, SummonData data) {
-        bigWheel.GetChild(currentSelectionIndex).GetComponent<Image>().sprite = defaultBg;
+        if (IsWithinSlots(currentSelectionIndex)) {
+            towerSlots[currentSelectionIndex].ToggleSelection(false);
+        }
 
         if (type == SummonType.None) {
             Hide();
             return;
         }
 
-        summonIndexDict.TryGetValue(data, out currentSelectionIndex);
+        bool hasSelection = summonIndexDict.TryGetValue(data, out currentSelectionIndex)
+                            && IsWithinSlots(currentSelectionIndex);
+        if (hasSelection) {
+            towerSlots[currentSelectionIndex].ToggleSelection(true);
 
-        bigWheel.GetChild(currentSelectionIndex).GetComponent<Image>().sprite = selectBg;
-
-        if (summonAngleDict.TryGetValue(data, out int targetRotation)) {
-            if (rotateWheelCoroutine != null) {
-                StopCoroutine(rotateWheelCoroutine);
+            if (summonAngleDict.TryGetValue(data, out int targetRotation)) {
+                if (rotateWheelCoroutine != null) {
+                    StopCoroutine(rotateWheelCoroutine);
+                }
+                rotateWheelCoroutine = StartCoroutine(RotateWheelToAngle(targetRotation));
             }
-            rotateWheelCoroutine = StartCoroutine(RotateWheelToAngle(targetRotation));
+            animator.SetBool("isSelect", true);
+        } else {
+            Hide();
         }
+    }
 
-        animator.SetBool("isSelect", true);
+    private bool IsWithinSlots(int index) {
+        return currentSelectionIndex < towerSlots.Length
+               && currentSelectionIndex >= 0;
     }
 
     private IEnumerator RotateWheelToAngle(float targetAngle) {
-        float duration = 1f;
-        float elapsedTime = 0f;
+        float elapsedTime = 0;
         float startAngle = bigWheel.localEulerAngles.z;
 
         if (Mathf.Abs(targetAngle - startAngle) > 180f) {
@@ -125,9 +120,9 @@ public class TowerWheel : MonoBehaviour
             }
         }
 
-        while (elapsedTime < duration) {
+        while (elapsedTime < rotateDuration) {
             elapsedTime += Time.deltaTime;
-            float currentAngle = Mathf.Lerp(startAngle, targetAngle, elapsedTime / duration);
+            float currentAngle = Mathf.Lerp(startAngle, targetAngle, elapsedTime / rotateDuration);
             bigWheel.localEulerAngles = new Vector3(bigWheel.localEulerAngles.x, bigWheel.localEulerAngles.y, currentAngle);
             smallWheel.localEulerAngles = new Vector3(smallWheel.localEulerAngles.x, smallWheel.localEulerAngles.y, -currentAngle);
             foreach (Transform child in bigWheel) {
