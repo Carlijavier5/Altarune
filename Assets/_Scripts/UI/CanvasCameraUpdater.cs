d97bd10cb5642cbf45da7ed7c34e6076ea4292c4
Carlos Suarez Barrera <carlijavier5@gmail.com>
2025-11-12 13:45:13 -0500

    UI Camera Stack

--- Assets/_Scripts/UI/CanvasCameraUpdater.cs
+++ Assets/_Scripts/UI/CanvasCameraUpdater.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CanvasCameraUpdater : MonoBehaviour {
     [SerializeField] private Canvas canvas;
+    private Camera uiCamera;
 
-    void Update() {
-        canvas.worldCamera = Camera.main;
+    void Awake() {
+        UniversalAdditionalCameraData cameraData = Camera.main.GetUniversalAdditionalCameraData();
+        if (cameraData.cameraStack.Count == 1) {
+            uiCamera = cameraData.cameraStack[0];
+        }
+        canvas.worldCamera = uiCamera;
     }
 }
